d635ade773f291ff9793f57105bdd02f74e6f204
timothyseguiawork <timothyseguiawork@gmail.com>
2024-09-08 22:02:36 -0400

    Add files via upload

---
+++ Exercise-Understanding-Null/script.js
@@ -0,0 +1,14 @@
+let score = undefined;
+let playerName = undefined;
+
+if (score == undefined){
+  console.log("yo this ain't right");
+};
+
+let player = null; 
+
+if (player?.score > 5) {
+  console.log("YOU WIN");
+} else {
+  console.log("YOUR TURN");
+};
